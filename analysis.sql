a3a05b09751b03e1692554938b535332b17053d4
parulgupta7 <parulguptainsan@gmail.com>
2017-05-23 00:19:26 +0530

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -1,3 +1,6 @@
+-- Connect to news database
+\c news;
+
 -- Droping already existing views with same names
 DROP VIEW IF EXISTS author_view CASCADE;
 DROP VIEW IF EXISTS author_view1 CASCADE;
@@ -7,9 +10,6 @@ DROP VIEW IF EXISTS error_view1 CASCADE;
 DROP VIEW IF EXISTS error_view2 CASCADE;
 DROP VIEW IF EXISTS error_view3 CASCADE;
 
--- Connect to news database
-\c news;
-
 -- Views required for implementation
 create view author_view as select title,count(title) as views from log,articles where path=concat('/article/',articles.slug) group by title order by views desc;
 create view author_view1 as select authors.id, articles.title from authors, articles where articles.author = authors.id;
@@ -17,5 +17,3 @@ create view author_view2 as select author_view1.id, author_view1.title, author_v
 create view error_view1 as select time::timestamp::date, count(status) as total from log group by time::timestamp::date order by time::timestamp::date;
 create view error_view2 as select time::timestamp::date, count(status) as fault from log where status = '404 NOT FOUND' group by time::timestamp::date;
 create view error_view3 as select error_view1.time, (fault::float * 100)/( total::float) as error_percent from error_view1, error_view2 where error_view1.time = error_view2.time;
-
-
